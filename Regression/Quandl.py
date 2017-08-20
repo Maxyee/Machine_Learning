15688779b8772272f76d3cad1811bdffc8ac21b5
Julhas <mdjulhashossainmohon@gmail.com>
2017-08-21 00:26:26 +0600

    Learning the Regression algorithm and machine learning things and enlarge the knowledge into this machine learning sector

--- Regression/Quandl.py
+++ Regression/Quandl.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import quandl
 
-db = quandl.get("WIKI/GOOGL")
+df = quandl.get("WIKI/GOOGL")
 
-print(db.head())
+df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
+
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+print(df.head())
